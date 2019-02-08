76baaf83029ecaddf5780bb445d6755d6f8575bf
Yifan Lu <lvan0707@outlook.com>
2019-02-08 10:48:22 +1000

    first servlet demo created

---
+++ Servlet/ServletDemo1/src/Add.java
@@ -0,0 +1,35 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Add
+ */
+@WebServlet("/Add")
+public class Add extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public void service(HttpServletRequest req, HttpServletResponse res) {
+		int num1 = Integer.parseInt(req.getParameter("num1"));
+		int num2 = Integer.parseInt(req.getParameter("num2"));
+		
+		int result = num1 + num2;
+		
+		try {
+			PrintWriter writer = res.getWriter();
+			writer.println(result);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
